dc467666b6eb077e968c6e4d663b1110b2b2c245
GitHub Action <action@github.com>
2021-03-23 13:42:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/aurimasniekis/maikuroideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.aurimasniekis.maikuroideaplugin.services
+
+import com.github.aurimasniekis.maikuroideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/aurimasniekis/maikuroideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aurimasniekis/maikuroideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aurimasniekis.maikuroideaplugin.services
 
+import com.github.aurimasniekis.maikuroideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
